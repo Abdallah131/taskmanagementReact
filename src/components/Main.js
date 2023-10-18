14b47ff6e9f2ccb1253c030effccc6e602c93d73
Hypex <zaghouane.abdallah@gmail.com>
2023-10-18 08:58:18 +0100

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Task from "./Task";
 
 export default function Main() {
+
+    const[data,setData] = useState({
+        text : "",
+        dueDate : ""    
+    })
+    
+    const [formState,setFormState] = useState(false)
+
+    // TO RETRIEVE TASKS FROM DB
+    // const [tasks,setTasks] = useState([])
+    // useEffect(()=> {
+    //     fetch("/taskList")
+    //         .then(res=>res.json())
+    //         .then(data=>setTasks(data))
+    // },[])
+
+    function handleFormState() {
+      setFormState(prevFormState => !prevFormState)
+    }
+
+    console.log(formState)
+
+    function handleChange(e) {
+        const { value, name } = e.target;
+        if (name === "dueDate") {
+          const selectedDate = new Date(value);
+          const formattedDate = `${selectedDate.getFullYear()}-${
+            String(selectedDate.getMonth() + 1).padStart(2, "0")
+          }-${String(selectedDate.getDate()).padStart(2, "0")}`;
+      
+          setData(prevData => ({
+            ...prevData,
+            [name]: formattedDate
+          }));
+        } else {
+          setData(prevData => ({
+            ...prevData,
+            [name]: value
+          }));
+        }
+      }
+      
     return (
         <div className="Container">
             <div className="Title">
@@ -9,9 +51,35 @@ export default function Main() {
             </div>
             <div className="Header">
                     <h2>This Month</h2>
-                    <button>Add Task</button>
+                    <button onClick={handleFormState}>Add Task</button>
             </div>
             <div className="Main">
+                <div className="Form--Container" style={{ display: formState ? "block" : "none" }}>
+                  <h1>Add Task</h1>
+                  <form action="/addTask" method="post">
+                          <textarea 
+                          placeholder='Task'
+                          type='text'
+                          name="text"
+                          value={data.text}
+                          onChange={handleChange}
+                          required
+                          /><br/>
+                          <span>Due Date : </span><input 
+                          placeholder='Due Date'
+                          type='date'
+                          name="dueDate"
+                          value={data.dueDate}
+                          onChange={handleChange}
+                          required
+                          /><br/><br/>
+                          <button>Add Task</button>
+                  </form>
+                </div>
+                <Task />
+                <Task />
+                <Task />
+                <Task />
                 <Task />
                 <Task />
             </div>
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,20 +3,27 @@ import More from '../images/viewmoreicon.png'
 export default function Task() {
     const [status , setStatus] = useState("On Going")
 
-    let styles = {}
+    let backgroundStyle = {}
+    let colorStyle = {}
     if(status === "On Going") {
-        styles = {
-            backgroundColor : "#0984E3",
+        backgroundStyle = {
+            backgroundColor : "#0984E3"
+        }
+        colorStyle = {
             color : "#0984E3"
         }
     }else if(status === "Completed") {
-        styles = {
-            backgroundColor : "#00B894",
+        backgroundStyle = {
+            backgroundColor : "#00B894"
+        }
+        colorStyle = {
             color : "#00B894"
         }
     }else {
-        styles = {
-            backgroundColor : "#da5656",
+        backgroundStyle = {
+            backgroundColor : "#da5656"
+        }
+        colorStyle = {
             color : "#da5656"
         }
     }
@@ -35,14 +42,14 @@ export default function Task() {
 
     return (
         <div className="Task--Container">
-            <div className="Hor--Bar" style={styles}></div>
+            <div className="Hor--Bar" style={backgroundStyle}></div>
             <div className="Task--Elements">
                 <div className="Task--Main">
                     <p>Comfort reached gay perhaps chamber his six detract besides add. Moonlight newspaper up he it enjoyment agreeable depending. Timed voice share led his widen noisy young. On weddings believed laughing although material do exercise of. Up attempt offered ye civilly so sitting to. She new course get living within elinor joy. She her rapturous suffering.</p>
                     <img src={More} alt="blyat"/>
                 </div>
                 <div className="Task--Lower">
-                    <p onClick={handleStatus} style={{cursor:"pointer"}}>Status : {status}</p>
+                    <p onClick={handleStatus} style={{cursor:"pointer"}}>Status : <span style={colorStyle}>{status}</span></p>
                     <p>Due date : 31/12/2023</p>
                 </div>
             </div>
